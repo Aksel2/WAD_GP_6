595b740656efd1b065252f87128081cbb685ebb9
Aksel Õim <akseloim@gmail.com>
2022-11-02 21:41:15 +0200

    Javascript for index.html and posts json

--- js/index.js
+++ js/index.js
@@ -1,58 +1,95 @@
-window.onload = function () {
+function dateFormatter(date){
+
+  let dateString = date;
+  let dateParts = dateString.split("/");
+  let dateObject = new Date(+dateParts[2], dateParts[1]-1, +dateParts[0])
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+  const day = dateObject.getDate();
+  const month = monthNames[dateObject.getMonth()];
+  const year = dateObject.getFullYear();
+  return day + ' ' + month + ', ' + year;
+}
+
+function loadPosts(){
+
   fetch("../res/json/posts.json")
     // Whenever the promise is resovled
     .then((response) => response.json()) // It's binary by default, let's say that we want it in json format.
 
     .then((posts) => {
-        console.log(posts);
-        for (i of posts){
-          console.log("data", i.post);
-          
-        }
-
+      jsonData = posts;
+   
+        
       for (post of posts){
-        const container = document.createElement("container"); // This contains the whole post including header and footer. aligns it
-        const card = document.createElement("card"); // This contains the post info
-        const cardHeading = document.createElement("card-heading");  // Contains the post header. Profile picture and date
-        const box = document.createElement("box"); // Contains the profile picture of the post heading
-        const textRight = document.createElement("right"); // This aligns the post header text to the right
-        const cardFooter = document.createElement("card-footer"); // Thumbs up reaction goes here
+        const div_container = document.createElement("div"); // This contains the whole post including header and footer.
+        const div_card = document.createElement("div"); // This contains the post info
+        const div_cardHeading = document.createElement("div");  // Contains the post header. Profile picture and date
+        const div_box = document.createElement("div"); // Contains the profile picture of the post heading
+        const div_right = document.createElement("div"); // This aligns the post header text to the right
+        const div_cardFooter = document.createElement("div"); // Thumbs up reaction goes here
         
-        const date = document.createElement("date");
-        date.textContent = post.date;
-        date.classList.add("date");
-        console.log(post.date);
-        textRight.appendChild(date);
+        const span = document.createElement("span");
+        span.textContent = dateFormatter(post.post.date);
+        div_right.classList.add("right");
+        div_right.appendChild(span);
+        const pc = document.createElement("img");
+        pc.src = post.post.profile;
+        div_box.appendChild(pc);
 
-        const profilePic = document.createElement("profilePic");
-        profilePic.textContent = post.profilePicture;
-        profilePic.classList.add("profilePic");
-        box.appendChild(profilePic);
+        const img = document.createElement("img");
+        img.src = post.post.img;
+        div_box.classList.add("box");
+        div_box.appendChild(div_right);
+        div_cardHeading.classList.add("card-heading");
+        div_cardHeading.appendChild(div_box);
 
-        const postPicture = document.createElement("postPicture");
-        postPicture.textContent = post.postPicture;
-        postPicture.classList.add("postPicture");
-        card.appendChild(postPicture);
+        const p = document.createElement("p");
+        p.textContent = post.post.message;
+        div_card.classList.add("card");
+     
 
-        const postText = document.createElement("postText");
-        postText.textContent = post.postBody;
-        postText.classList.add("postText");
-        card.appendChild(postText);
-
-        const footerImage = document.createElement("footerImage");
-        footerImage.textContent = post.footerPicture;
-        footerImage.classList.add("footerImage");
-        cardFooter.appendChild(footerImage);
-
-        card.appendChild(cardHeading);
-        card.appendChild(box);
-        card.appendChild(cardFooter);
-        container.appendChild(card);
+        const footer = document.createElement("footer");
         
+        const react = document.createElement("img");
+        react.src = "https://cdn.worldvectorlogo.com/logos/thumbs-up-facebook.svg";
+        div_cardFooter.appendChild(react);
 
+        div_cardFooter.classList.add("card-footer");
+        div_cardFooter.append(footer);
+        
+        div_right.appendChild(span);
+        div_box.appendChild(div_right);
 
+        div_cardHeading.appendChild(div_box);
+        div_cardHeading.classList.add("card-heading")
+        
+        // post heading
+        div_card.appendChild(div_cardHeading);
+        
+        // Postituse sisu
+        div_card.appendChild(img);
+        div_card.appendChild(p);
+        
+        // post footer
+        div_cardFooter.appendChild(footer);
+        div_card.appendChild(div_cardFooter);
+
+        div_container.appendChild(div_card);
+        div_container.classList.add("container"); 
+       
+        const body = document.querySelector('body');
+        body.appendChild(div_container);
+        
 
       }
+      // page footer
+      const body = document.querySelector('body');
+      const div_containerFooter = document.createElement("div");
+      div_containerFooter.classList.add("container-footer");
+      body.appendChild(div_containerFooter);
+      
 
     })
     .catch((err) => {
@@ -60,31 +97,102 @@ window.onload = function () {
 
     })
     .finally(() => {
-        console.log("Hello");
+      console.log("Done");
+   
     });
-};
-/*
-<div class="container">
-        <div class="card">
-          <div class="card-heading">
-            <div class="box">
-              <img src="../res/images/profile2.png" alt="profile picture" />
-              <div class="right">
-                <span>3 October, 2022</span>
-              </div>
-            </div>
-          </div>
-          <img src="../res/images/raekoda.JPG" alt="Image of Tartu raekoda" />
-          <p>Käisin Tartu raekojas!</p>
-          <div class="card-footer">
-            <footer>
-              <img
-                src="https://cdn.worldvectorlogo.com/logos/thumbs-up-facebook.svg"
-                alt="Thumbs up"
-              />
-            </footer>
-          </div>
-        </div>
-      </div>
+}
 
-*/
+
+
+window.onload = function () {
+
+    
+  // Fetching post and user data
+    const fetchData = async () => {
+      try {
+        const responsesJSON = await Promise.all([
+            //fetch('https://myjson.dit.upm.es/api/bins/3k72?fbclid=IwAR28FflHyklfamoQsqEn3eOWRJBVX-cry0aWdx5DrhIn3Mzj-4sR3gnbo2g'),
+            fetch('../res/json/users.json'),
+            fetch('../res/json/posts.json')
+        ]);
+        // Reads the JSONs into constants
+        const [users, posts] = await Promise.all(responsesJSON.map(r => r.json()));
+      
+        for (post of posts){
+          const div_container = document.createElement("div"); // This contains the whole post including header and footer.
+          const div_card = document.createElement("div"); // This contains the post info
+          const div_cardHeading = document.createElement("div");  // Contains the post header. Profile picture and date
+          const div_box = document.createElement("div"); // Contains the profile picture of the post heading
+          const div_right = document.createElement("div"); // This aligns the post header text to the right
+          const div_cardFooter = document.createElement("div"); // Thumbs up reaction goes here
+          
+          // Adding the card header, which contains the date and profile image
+          const span = document.createElement("span");
+          span.textContent = dateFormatter(post.post.date);
+          div_right.classList.add("right");
+          div_right.appendChild(span);
+          const pc = document.createElement("img");
+          // Getting user profile picture
+          pc.src = users[post.userId - 1].picture;
+          div_box.appendChild(pc);
+  
+          // Adding post image and text
+          const img = document.createElement("img");
+          img.src = post.post.img;
+          div_box.classList.add("box");
+          div_box.appendChild(div_right);
+          div_cardHeading.classList.add("card-heading");
+          div_cardHeading.appendChild(div_box);
+  
+          const p = document.createElement("p");
+          p.textContent = post.post.message;
+          div_card.classList.add("card");
+          
+          // Adding post footer which contains the reaction thumbs up
+          const footer = document.createElement("footer");
+          const react = document.createElement("img");
+          react.src = "https://cdn.worldvectorlogo.com/logos/thumbs-up-facebook.svg";
+          div_cardFooter.appendChild(react);
+  
+          div_cardFooter.classList.add("card-footer");
+          div_cardFooter.append(footer);
+          
+          div_right.appendChild(span);
+          div_box.appendChild(div_right);
+  
+          div_cardHeading.appendChild(div_box);
+          div_cardHeading.classList.add("card-heading")
+          
+          // post heading
+          div_card.appendChild(div_cardHeading);
+          
+          // Postituse sisu
+          div_card.appendChild(img);
+          div_card.appendChild(p);
+          
+          // post footer
+          div_cardFooter.appendChild(footer);
+          div_card.appendChild(div_cardFooter);
+  
+          div_container.appendChild(div_card);
+          div_container.classList.add("container"); 
+         
+          const body = document.querySelector('body');
+          body.appendChild(div_container);
+          
+  
+        }
+        // page footer
+        const body = document.querySelector('body');
+        const div_containerFooter = document.createElement("div");
+        div_containerFooter.classList.add("container-footer");
+        body.appendChild(div_containerFooter);
+
+
+      } catch (err) {
+        throw err;
+      }
+    };
+    
+    fetchData();
+};

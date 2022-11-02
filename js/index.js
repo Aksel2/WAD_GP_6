00e48e9d0e53f9016dbdb3854f895ace2fce44b5
Aksel Õim <akseloim@gmail.com>
2022-11-02 22:08:27 +0200

    Removed excess code and added some comments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+// Formats DD/MM/YYYY to DD monthName, YYYY
 function dateFormatter(date){
 
   let dateString = date;
@@ -12,100 +14,8 @@ function dateFormatter(date){
   return day + ' ' + month + ', ' + year;
 }
 
-function loadPosts(){
-
-  fetch("../res/json/posts.json")
-    // Whenever the promise is resovled
-    .then((response) => response.json()) // It's binary by default, let's say that we want it in json format.
-
-    .then((posts) => {
-      jsonData = posts;
-   
-        
-      for (post of posts){
-        const div_container = document.createElement("div"); // This contains the whole post including header and footer.
-        const div_card = document.createElement("div"); // This contains the post info
-        const div_cardHeading = document.createElement("div");  // Contains the post header. Profile picture and date
-        const div_box = document.createElement("div"); // Contains the profile picture of the post heading
-        const div_right = document.createElement("div"); // This aligns the post header text to the right
-        const div_cardFooter = document.createElement("div"); // Thumbs up reaction goes here
-        
-        const span = document.createElement("span");
-        span.textContent = dateFormatter(post.post.date);
-        div_right.classList.add("right");
-        div_right.appendChild(span);
-        const pc = document.createElement("img");
-        pc.src = post.post.profile;
-        div_box.appendChild(pc);
-
-        const img = document.createElement("img");
-        img.src = post.post.img;
-        div_box.classList.add("box");
-        div_box.appendChild(div_right);
-        div_cardHeading.classList.add("card-heading");
-        div_cardHeading.appendChild(div_box);
-
-        const p = document.createElement("p");
-        p.textContent = post.post.message;
-        div_card.classList.add("card");
-     
-
-        const footer = document.createElement("footer");
-        
-        const react = document.createElement("img");
-        react.src = "https://cdn.worldvectorlogo.com/logos/thumbs-up-facebook.svg";
-        div_cardFooter.appendChild(react);
-
-        div_cardFooter.classList.add("card-footer");
-        div_cardFooter.append(footer);
-        
-        div_right.appendChild(span);
-        div_box.appendChild(div_right);
-
-        div_cardHeading.appendChild(div_box);
-        div_cardHeading.classList.add("card-heading")
-        
-        // post heading
-        div_card.appendChild(div_cardHeading);
-        
-        // Postituse sisu
-        div_card.appendChild(img);
-        div_card.appendChild(p);
-        
-        // post footer
-        div_cardFooter.appendChild(footer);
-        div_card.appendChild(div_cardFooter);
-
-        div_container.appendChild(div_card);
-        div_container.classList.add("container"); 
-       
-        const body = document.querySelector('body');
-        body.appendChild(div_container);
-        
-
-      }
-      // page footer
-      const body = document.querySelector('body');
-      const div_containerFooter = document.createElement("div");
-      div_containerFooter.classList.add("container-footer");
-      body.appendChild(div_containerFooter);
-      
-
-    })
-    .catch((err) => {
-      console.log("Error");
-
-    })
-    .finally(() => {
-      console.log("Done");
-   
-    });
-}
-
-
 
 window.onload = function () {
-
     
   // Fetching post and user data
     const fetchData = async () => {
@@ -125,6 +35,7 @@ window.onload = function () {
           const div_box = document.createElement("div"); // Contains the profile picture of the post heading
           const div_right = document.createElement("div"); // This aligns the post header text to the right
           const div_cardFooter = document.createElement("div"); // Thumbs up reaction goes here
+          
           
           // Adding the card header, which contains the date and profile image
           const span = document.createElement("span");

fdf2e9a7976a1fdf6ba1efebc8910e0a9dd4da93
Aksel Õim <akseloim@gmail.com>
2022-12-12 15:52:12 +0200

    Seperated front and back

---
+++ Backend/README.md
@@ -0,0 +1,31 @@
+# WAD2022-Lab12-Backend
+
+## Pre-requisites
+Node.js \& NPM
+
+## Do not forget to add the password: "" of your database in database.js
+
+## Clone the repository
+```
+git clone https://github.com/M-Gharib/WAD2022-Lab12-Backend.git
+```
+
+## Navigate to the project directory
+```
+cd <project name>
+```
+
+## Install dependencies
+```
+npm install
+```
+
+## Run the project
+```
+npm start
+```
+
+or 
+```
+npm server.js
+```
---
+++ Backend/server.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const pool = require('./database');
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+//The express.json() function is a built-in middleware function in Express. 
+//It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+
+//"async and await make promises easier to write"
+// async makes a function return a Promise
+// The keyword async before a function makes the function return a promise.
+// Syntax:  "async(req, res) => {}"
+// await makes a function wait for a Promise
+// The await keyword can only be used inside an async function.
+// The await keyword makes the function pause the execution and wait for a resolved promise before it continues
+// Syntax:  "async(req, res) => {let value = await promise}"
+
+/* 
+// Task 1
+app.post('/api/posts', async(req, res) => {
+    try {
+        console.log("a post request has arrived");
+        const post = req.body;
+        const newpost = await pool.query(
+            "INSERT INTO posttable(title, body, urllink) values ($1, $2, $3)    RETURNING*", [post.title, post.body, post.urllink]
+            // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body, post.urllink)
+            // The RETURNING keyword in PostgreSQL allows returning a value from the insert or update statement.
+            // using "*" after the RETURNING keyword in PostgreSQL, will return everything
+        );
+        res.json(newpost);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+*/
+
+/* 
+// Task 2
+app.get('/api/posts', async(req, res) => {
+    try {
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posttable"
+        );
+        res.json(posts.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+ */
+
+/* 
+// Task 3
+app.get('/api/posts/:id', async(req, res) => {
+    try {
+        console.log("get a post with route parameter  request has arrived");
+        // The req.params property is an object containing properties mapped to the named route "parameters". 
+        // For example, if you have the route /posts/:id, then the "id" property is available as req.params.id.
+        const { id } = req.params; // assigning all route "parameters" to the id "object"
+        const posts = await pool.query( // pool.query runs a single query on the database.
+            //$1 is mapped to the first element of { id } (which is just the value of id). 
+            "SELECT * FROM posttable WHERE id = $1", [id]
+        );
+        res.json(posts.rows[0]); // we already know that the row array contains a single element, and here we are trying to access it
+        // The res.json() function sends a JSON response. 
+        // This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using the JSON.stringify() method.
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+*/
+
+/* 
+// Task 4
+app.put('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const post = req.body;
+        console.log("update request has arrived");
+        const updatepost = await pool.query(
+            "UPDATE posttable SET (title, body, urllink) = ($2, $3, $4) WHERE id = $1", [id, post.title, post.body, post.urllink]
+        );
+        res.json(updatepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+ */
+
+/* 
+// Task 5
+app.delete('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        //const post = req.body; // we do not need a body for a delete request
+        console.log("delete a post request has arrived");
+        const deletepost = await pool.query(
+            "DELETE FROM posttable WHERE id = $1", [id]
+        );
+        res.json(deletepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+*/
+
+app.listen(port, () => {
+    console.log("Server is listening to port " + port)
+});
